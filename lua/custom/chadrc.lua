7712091646d34211b5239414ab6ac15d6b4e47a3
Zach Podbielniak <z.podbielniak@disroot.org>
2024-03-03 00:35:15 -0500

    Configs

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,7 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { theme = 'doomchad' }
+M.plugins = "custom.plugins"
+
+return M
---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,41 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- bash
+lspconfig.bashls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"sh"},
+  cmd = {"bash-language-server", "start"},
+  single_file_support = true
+})
+
+-- docker
+lspconfig.dockerls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"dockerfile", "containerfile"},
+  cmd = {"docker-langserver", "--stdio"},
+  single_file_support = true,
+  --root_dir = vim.fs.dirname(vim.fs.find({'Containerfile', 'Dockerfile'}, { upward = true })[1]),
+})
+
+-- python
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python", "py"},
+  cmd = {"pyright-langserver", "--stdio"},
+  single_file_support = true
+})
+
+-- c 
+lspconfig.clangd.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"c", "cpp", "cc", "objc", "objcpp", "cuda", "proto"},
+  cmd = {"clangd"},
+  single_file_support = true
+})
