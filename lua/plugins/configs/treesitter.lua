7695ed101bea089b98f53e57027cf06721b82842
Zach Podbielniak <z.podbielniak@disroot.org>
2024-09-15 07:47:34 -0400

    feat(just): add just treesitter plugin

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -9,4 +9,15 @@ local options = {
   indent = { enable = true },
 }
 
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.just = {
+  install_info = {
+    url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+    files = { "src/parser.c", "src/scanner.c" },
+    branch = "main",
+    -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+  },
+  maintainers = { "@IndianBoy42" },
+}
+
 return options
