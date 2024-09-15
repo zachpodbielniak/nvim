c0eb623b2cae7264deb2b76f31842f31ae739cca
Zach Podbielniak <z.podbielniak@disroot.org>
2024-09-15 10:45:58 -0400

    feat(plugin): add neorg

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -41,6 +41,45 @@ local plugins = {
             require('transparent').clear_prefix('NeoTree')
             require('transparent').clear_prefix('lualine')
         end,
+    },
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+        config = true,
+        lazy = false,
+        opts = {
+            rocks = { "lua-utils" }
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-neotest/nvim-nio",
+            "nvim-neorg/lua-utils.nvim",
+            "nvim-lua/plenary.nvim",
+            "pysan3/pathlib.nvim"
+        }
+    },
+    {
+        "nvim-neorg/neorg",
+        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "*", -- Pin Neorg to the latest stable release
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behavior 
+                    ["core.concealer"] = {}, -- Adds pretty icons to documents
+                    ["core.dirman"] = { -- Manages Neorg WorkspaceSymbol
+                        config = {
+                            workspaces = {
+                                personal = "~/Documents/notes/personal",
+                                work = "~/Documents/notes/work"
+                            },
+                            default_workspace = "personal"
+                        }
+                    }
+                }
+            })
+        end,
+        dependencies = { "luarocks.nvim", "nvim-treesitter" }
     }
 }
 
